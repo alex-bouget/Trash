fa34e2f92b4adb0775a51e61c784132a2de03769
MisterMine01 <pokemovie06@gmail.com>
2021-12-31 15:42:16 +0100

    add console interpreter

--- python/LineUp/interpreter/interpreter.py
+++ python/LineUp/interpreter/interpreter.py
@@ -13,6 +13,27 @@ class Interpreter:
     def set_class(self, name, value):
         self.global_class[name] = value
 
+    def console(self):
+        print("By MisterMine01\n\n")
+        t = ""
+        while True:
+            while len(t) == 0 or t[-1] != ";":
+                t = t+input(">>")
+            t = " ".join(t.split(";"))
+            t = " ".join(t.split("\n"))
+            if "".join(t.split(" ")) == "exit":
+                break
+            elif "".join(t.split(" ")) == "loaded":
+                print("class_loaded:")
+                for i in self.global_class.keys():
+                    print(i)
+            elif "".join(t.split(" ")) == "var":
+                for i in self.global_variable.keys():
+                    print(i)
+            else:
+                print(Line(t).execute(self.global_variable, self.global_class))
+            t = ""
+
     def execute(self, file):
         for i in open(file).read().split(";"):
             data = Line(" ".join(i.split("\n"))).execute(self.global_variable, self.global_class)
--- python/main_exe.py
+++ python/main_exe.py
@@ -4,6 +4,14 @@ import getopt
 import sys
 import os
 
+def usage():
+    print("Usage: \n" +
+          "-m: lum file, Modules loaded (.lum)\n" +
+          "-d: modules dir\n" +
+          "[-f]: lup file, LineUp code\n" +
+          "if -f not use, open the interpreter"
+          )
+
 if __name__ == "__main__":
     opts, args = getopt.getopt(sys.argv[1:], "f:m:d:")
 
@@ -12,11 +20,22 @@ if __name__ == "__main__":
     }
     lup = None
 
+    if len(opts) <= 0:
+        usage()
+        exit(0)
+
     for opt, arg in opts:
         if opt in ['-f']:
-            lup=os.path.abspath(arg)
+            lup = os.path.abspath(arg)
         elif opt in ['-m']:
             data["lum"] = os.path.abspath(arg)
         elif opt in ['-d']:
             data["m_dir"] = os.path.abspath(arg)
-    LineUp(**data).execute(lup)
+        else:
+            usage()
+            exit(0)
+    if lup is None:
+        print("LineUp Interpreter 0.12.31")
+        LineUp(**data).console()
+    else:
+        LineUp(**data).execute(lup)
