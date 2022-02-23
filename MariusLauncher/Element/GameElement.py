0b8dd3c66f6ddede1e0accb7d3b1961281c8eae7
MisterMine01 <pokemovie06@gmail.com>
2022-02-23 19:52:32 +0100

    Update Legendary.exe, Element.py, and 28 more files...

--- MariusLauncher/Element/GameElement.py
+++
@@ -1,6 +0,0 @@
-from .WidgetElement.ButtonElement import ButtonElement
-
-
-class GameElement(ButtonElement):
-    def __init__(self, master, json, content_number):
-        super().__init__(master, json, content_number)
--- MariusLauncher/Element/__init__.py
+++
@@ -1,9 +0,0 @@
-from .WidgetElement.CanvasElement import CanvasElement
-from .WidgetElement.ButtonElement import ButtonElement
-from .GameElement import GameElement
-
-element = {
-    "canvas": CanvasElement,
-    "button": ButtonElement,
-    "giglygame": GameElement
-}
