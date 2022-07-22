83c15adeda697da14a3825f5c42501bb153cd98b
MisterMine01 <abouget68@gmail.com>
2022-07-22 19:50:06 +0200

    add comment

--- KromBlast/kromblast/exceptions.py
+++ KromBlast/kromblast/exceptions.py
@@ -1,11 +1,15 @@
 class ConfigJsonError(Exception):
+    """Appen when the config.ini value is not json serializable"""
     pass
 
 class ModeError(Exception):
+    """Appen when the mode of file is not valid"""
     pass
 
 class InvalidWindowParameter(Exception):
+    """Appen when a window parameter does not exist"""
     pass
 
 class SubPluginException(Exception):
+    """Appen when a subplugin is detected (not implemented)"""
     pass
