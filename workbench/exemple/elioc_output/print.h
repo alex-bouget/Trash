45c3943c6f50458a1fc90f9f781be0769ba35e18
MisterMine01 <pokemovie06@gmail.com>
2022-10-10 19:15:07 +0200

    add more for workbench

---
+++ workbench/exemple/elioc_output/print.h
@@ -0,0 +1,6 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+void afficher(char* v0);
+
+#endif
---
+++ workbench/exemple/print.py
@@ -0,0 +1,6 @@
+"""
+@!type python
+@!decode elioc.python-0.1
+"""
+def afficher(x: str) -> None:
+    print(x)
